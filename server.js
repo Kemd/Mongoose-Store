48e2d4602faa3eb38e18e825c8ddc2df827f4228
kemd <kemd91@gmail.com>
2022-05-24 16:51:20 -0400

    added routes, index, new, setup forms to create product

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 require('dotenv').config()
 const express = require('express')
 const Product = require('./models/products')
+// === seed data for testing, delete after === 
+const productSeed = require('./models/productSeed')
 const mongoose = require('mongoose')
 const req = require('express/lib/request')
 
@@ -23,15 +25,45 @@ db.on('disconnected', () => console.log('Mongodb is disconnected'))
 app.use(express.urlencoded({ extended: false}))
 
 
-
-
 // === ROUTES start ===
 
+// Routes / Controllers
+// Seed for testing - delete later
+
+// Routes / Controllers
+// Seed
+
+app.get('/products/seed', (req, res) => {
+	Book.deleteMany({}, (error, seeds) => {});
+
+	Book.create(productSeed, (error, data) => {
+		res.redirect('/products');
+	});
+});
+
+
 // === INDEX ===
+app.get('/products', (req, res) => {
+    res.render('index.ejs')
+})
+
+
 // === New ===
+app.get('/products/new', (req, res) => {
+    res.render('new.ejs')
+})
 // === Delete ===
 // === Update ===
+
+
 // === Create ===
+app.post('/products', (req, res) => {
+    Product.create(req.body, (err, createdProduct) => {
+        res.redirect('/products')
+    })
+})
+
+
 // === Edit ===
 // === Show ===
 
